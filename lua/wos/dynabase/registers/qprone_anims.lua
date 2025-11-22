e375292825c4de422ccf1f63651e847a024f2c8c
VinierAardvark1 <jackprovine@gmail.com>
2025-11-21 21:25:13 -0600

    DynaBase fix
    I guess DynaBase saw files with the same name and had a stroke, so I removed them.
    They weren't needed, anyways.

--- lua/wos/dynabase/registers/qprone_anims.lua
+++ lua/wos/dynabase/registers/qprone_anims.lua
@@ -3,17 +3,17 @@ wOS.DynaBase:RegisterSource({
     Type = WOS_DYNABASE.EXTENSION,
 
     -- model paths per gender:
-    Shared = "models/police_ss.mdl",      -- default or neutral model
-    Female = "models/f_anm.mdl",    -- female-specific model (optional)
-    Male   = "models/m_anm.mdl"     -- optional if you have a male version
+    Shared = "models/player/wiltos/anim_extension_prone.mdl",      -- default or neutral model
+    Female = "models/player/wiltos/anim_extension_prone.mdl",    -- female-specific model (optional)
+    Male   = "models/player/wiltos/anim_extension_prone.mdl"     -- optional if you have a male version
 })
 
 hook.Add("PreLoadAnimations", "wOS.DynaBase.MountqProne", function(gender)
     if gender == WOS_DYNABASE.SHARED then
-        IncludeModel("models/police_ss.mdl")
+        IncludeModel("models/player/wiltos/anim_extension_prone.mdl")
     elseif gender == WOS_DYNABASE.FEMALE then
-        IncludeModel("models/f_anm.mdl")
+        IncludeModel("models/player/wiltos/anim_extension_prone.mdl")
     elseif gender == WOS_DYNABASE.MALE then
-        IncludeModel("models/m_anm.mdl")
+        IncludeModel("models/player/wiltos/anim_extension_prone.mdl")
     end
 end)
